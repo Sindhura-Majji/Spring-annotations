dabbe1cbdff79ad6ca2404c584c305477e8dcd22
Sindhura-Majji <sindhura.majji@gmail.com>
2019-07-14 19:00:45 +0530

    annotations task4

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,16 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
 
     //Actor Class Initialisation
@@ -26,5 +32,20 @@ public class Movie {
     }
 
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactory is: "+beanFactory);
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("The name of the bean is:" +beanName);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Implemented ApplicationContextAware");
+    }
 }
 
--- src/main/java/com/stackroute/domain/SpringConfig.java
+++ src/main/java/com/stackroute/domain/SpringConfig.java
@@ -4,9 +4,10 @@ package com.stackroute.domain;
 import org.springframework.context.annotation.*;
 
 @Configuration
-@ComponentScan("com.stackroute.domain")
+@ComponentScan("com.stackroute")
 @PropertySource("classpath:Actor-info.properties")
 public class SpringConfig {
+
 
 
 }
