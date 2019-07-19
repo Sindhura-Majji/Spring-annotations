18f85ea4ba95c9a8878dccc07adcc3d604fdfdc6
Sindhura-Majji <51950216+Sindhura-Majji@users.noreply.github.com>
2019-07-19 14:20:37 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -9,22 +9,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        
         //Using ApplicationContext
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         Movie movie1 = context.getBean(Movie.class);
         movie1.movie();
         Movie movie2=context.getBean(Movie.class);
         movie2.movie();
+        
         //checking bean scope
         System.out.println(movie1==movie2);
-
-
-
-
 
     }
 
